656d4f2b564541e0a36dc01fc062f94bf982324e
Beskrovniy Konstantin Evgenyevich <k.beskrovniy@tinkoff.ru>
2021-02-22 14:49:31 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const timers = document.querySelectorAll('.js-time');
+
+const players = document.querySelectorAll('.player');
+
+let activePlayerIndex = null;
+let activePlayerInterval = null;
+
+let timeBank = Array.from(players).map(() => 10 * 60 * 1000);
+
+let start = null;
+let playerStartTime = null;
+
+function render(timestamp) {
+    if (activePlayerIndex === null) {
+        return;
+    }
+    
+    if (start === null) {
+        start = timestamp;
+    }
+
+    const diff = timestamp - start;
+    const newTime = playerStartTime - diff;
+
+    timeBank[activePlayerIndex] = newTime;
+    timers[activePlayerIndex].textContent = `${new Date(newTime).getMinutes()}:${new Date(newTime).getSeconds()}`;
+
+    window.requestAnimationFrame(render);
+}
+
+players.forEach((player, index) => {
+    player.addEventListener('click', () => {
+        if (activePlayerIndex === null) {
+            window.requestAnimationFrame(render);
+        }
+
+        if (activePlayerIndex !== index) {
+            activePlayerIndex = index;
+            start = null;
+            playerStartTime = timeBank[index];
+        } else {
+            activePlayerIndex = null;
+        }
+    });
+});
+
+
+const reset = document.querySelector('.js-reset');
+
+reset.addEventListener('click', () => {
+    activePlayerIndex = null;
+    start = null;
+
+    timeBank = Array.from(players).map(() => 10 * 60 * 1000);
+
+    Array.from(timers).forEach((timer, index) => {
+        timer.textContent = `10:00`;
+    })
+});
